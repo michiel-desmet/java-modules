2255fd4b7a1aa39f0241256b5455c86583e87d85
Michiel de Smet <m.desmet@dictu.nl>
2024-10-03 13:38:10 +0200

    improved the module-info declarations

--- care/src/module-info.java
+++ care/src/module-info.java
@@ -1,4 +1,5 @@
 module zoo.animal.care {
     exports zoo.animal.care.details;
-    requires zoo.animal.feeding;
+    //whenever a modules requires zoo.animal.care, it should not specify zoo.animal.feeding. because it is part of the zoo.animal.care.
+    requires transitive zoo.animal.feeding;
 }
--- talks/src/module-info.java
+++ talks/src/module-info.java
@@ -1,8 +1,8 @@
 module zoo.animal.talks {
-    exports zoo.animal.talks.content;
+    //Only zoo.staff can access the content of zoo.animal.talks.content
+    exports zoo.animal.talks.content to zoo.staff;
     exports zoo.animal.talks.media;
     exports zoo.animal.talks.schedule;
 
-    requires zoo.animal.feeding;
-    requires zoo.animal.care;
+    requires transitive zoo.animal.care;
 }
